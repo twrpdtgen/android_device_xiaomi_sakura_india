2ea1e79bc763b8da1f2bfca1022b11ae83c9147e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-28 06:58:43 +0200

    sakura_india: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: xiaomi Model: Redmi 6 Pro Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sakura_india.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sakura_india-user \
+    omni_sakura_india-userdebug \
+    omni_sakura_india-eng
---
+++ omni_sakura_india.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from sakura_india device
+$(call inherit-product, device/xiaomi/sakura_india/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sakura_india
+PRODUCT_NAME := omni_sakura_india
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 6 Pro
+PRODUCT_MANUFACTURER := xiaomi
